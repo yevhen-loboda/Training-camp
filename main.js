cf48d932455a8bd9d546308277421b76ff9b8ac7
Yevhen Loboda <yevhen.loboda1986@gmail.con>
2017-01-13 13:18:30 +0200

    Show alert on press button

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 /**
  * Created by yevhen-loboda on 13.01.17.
  */
-var value1 = document.getElementById("num1").value;
-var value2 = document.getElementById("num2").value;
-alert("value1:"+value1);
-alert("value2:"+value2);
+function WebCalc() {
+    var value1 = document.getElementById("num1").value;
+    var value2 = document.getElementById("num2").value;
+    alert("value1:" + value1);
+    alert("value2:" + value2);
+}
+document.getElementById("btn").onclick = WebCalc;

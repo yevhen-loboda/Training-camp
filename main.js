ee6fbb55587fc8dcf64ffd8853546c7009db7cdc
Yevhen Loboda <yevhen.loboda1986@gmail.con>
2017-01-14 19:15:28 +0200

    Start server

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
  * Created by yevhen-loboda on 13.01.17.
  */
 function WebCalc() {
-    var value1 = document.getElementById("num1").value;
-    var value2 = document.getElementById("num2").value;
+    var value1 = Number(document.getElementById("num1").value);
+    var value2 = Number(document.getElementById("num2").value);
+    var sum = value1 + value2;
     alert("value1:" + value1);
     alert("value2:" + value2);
+    alert("Sum=" + sum);
 }
 document.getElementById("btn").onclick = WebCalc;

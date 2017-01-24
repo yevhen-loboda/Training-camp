d4b068ebd4c7945fc25d77fd3f118dc57f3299da
Yevhen Loboda <yevhen.loboda1986@gmail.con>
2017-01-24 17:45:51 +0200

    finish

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ var app = express();
 
 // создаём маршрут для главной страницы
 // http://localhost:8080/
-app.get('/', function(req, res) {
-    res.sendfile('index.html');
-});
+//app.get('/', function(req, res) {
+//    res.sendfile('index.html');
+//});
+app.use(express.static("ff"))
 
 // запускаем сервер на порту 8080
 app.listen(8080);
